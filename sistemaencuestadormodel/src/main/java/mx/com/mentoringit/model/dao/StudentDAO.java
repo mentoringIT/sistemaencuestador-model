0a4447d1108b22686bb43375563358f9d5746e45
Adan Santos <Adan Santos@192.168.1.80>
2017-02-11 14:29:55 -0600

    commit inicial

---
+++ sistemaencuestadormodel/src/main/java/mx/com/mentoringit/model/dao/StudentDAO.java
@@ -0,0 +1,51 @@
+package mx.com.mentoringit.model.dao;
+
+import java.io.Serializable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.util.List;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import mx.com.mentoringit.model.dto.StudentDTO;
+
+
+@Repository
+public class StudentDAO implements IStudent, Serializable {
+	
+	public StudentDAO(){};
+	
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	public void setJdbcTemplate(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public List<StudentDTO> select() {
+		String select = "select * from tbl_student";
+		List<StudentDTO> lAlumnos = this.jdbcTemplate.query(select, new RowMapper<StudentDTO>() {
+		            public StudentDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		            	StudentDTO student = new StudentDTO();
+		            	student.setId(rs.getInt("ID"));
+		            	student.setEmail(rs.getString("EMAIL"));
+		            	student.setName(rs.getString("NAME"));
+		            	student.setPhone(rs.getString("PHONE"));
+		            	student.setAddres(rs.getString("ADDRESS"));
+		            	student.setStatus(rs.getShort("STATUS"));
+		                return student;
+		            }
+		        });
+		
+		
+		
+		return lAlumnos;
+	}
+
+}
---
+++ sistemaencuestadormodel/src/main/java/mx/com/mentoringit/model/dao/UserDAO.java
@@ -0,0 +1,41 @@
+package mx.com.mentoringit.model.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import mx.com.mentoringit.model.dto.UserDTO;
+
+@Repository
+public class UserDAO implements IUser {
+
+	public UserDAO() {
+		// TODO Auto-generated constructor stub
+	}
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public UserDTO userLogin(UserDTO userDTO) throws Exception {
+		UserDTO user = this.jdbcTemplate.queryForObject(
+		        "SELECT username FROM tbl_users where  username = ? and password  = ? ",
+		        new Object[]{userDTO.getUsername(), userDTO.getPassword()},
+		        new RowMapper<UserDTO>() {
+		            public UserDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		                UserDTO user = new UserDTO();
+		                user.setUsername(rs.getString("username"));
+		                return user;
+		            }
+		        });
+		return user;
+	}
+}
